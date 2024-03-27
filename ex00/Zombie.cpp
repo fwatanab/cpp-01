93720c8535e4ade42f6bc72de8d130638c88c706
fwatanab <fwatanab@student.42tokyo.jp>
2024-03-28 06:10:28 +0900

    不要な関数の削除

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -10,12 +10,3 @@ Zombie::~Zombie() {
 void	Zombie::announce(void) {
 	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
-
-Zombie	*newZombie(std::string name) {
-	return new	Zombie(name);
-}
-
-void	randomChump(std::string name) {
-	Zombie	zom(name);
-	zom.announce();
-}
