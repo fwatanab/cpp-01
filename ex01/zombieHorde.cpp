92d7148fd039bcd67df013dbf657b375ebeaa0e2
fwatanab <fwatanab@student.42tokyo.jp>
2024-03-28 07:32:39 +0900

    複数のゾンビを動的に確保する関数

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -2,8 +2,10 @@
 
 Zombie	*zombieHorde(int N, std::string name) {
 	Zombie	*horde = new Zombie[N];
+
 	for (int i = 0; i < N; i++) {
 		horde[i] = Zombie(name);
 	}
-	return (horde);
+
+	return horde;
 }
